4440ba82ae97676177688421017bbb83867e465c
nswbmw <gxqzk@126.com>
2017-02-10 16:37:43 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+## appoint
+
+Another Promise implementation. Inspired by [lie](https://github.com/calvinmetcalf/lie).
+
+### Install
+
+```bash
+npm install appoint
+```
+
+### Usage
+
+```javascript
+var Promise = require('appoint');
+// or use the pollyfill
+require('appoint/polyfill');
+```
+
+### License
+
+MIT
---
+++ appoint.js
@@ -0,0 +1,236 @@
+'use strict';
+
+var immediate = require('immediate');
+
+function INTERNAL() {}
+function isFunction(func) {
+  return typeof func === 'function';
+}
+function isObject(obj) {
+  return typeof obj === 'object';
+}
+function isArray(arr) {
+  return Object.prototype.toString.call(arr) === '[object Array]';
+}
+
+var PENDING = 0;
+var FULFILLED = 1;
+var REJECTED = 2;
+
+module.exports = Promise;
+
+function Promise(resolver) {
+  if (!isFunction(resolver)) {
+    throw new TypeError('resolver must be a function');
+  }
+  this.state = PENDING;
+  this.value = void 0;
+  this.queue = [];
+  if (resolver !== INTERNAL) {
+    safelyResolveThenable(this, resolver);
+  }
+}
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+  if (!isFunction(onFulfilled) && this.state === FULFILLED ||
+    !isFunction(onRejected) && this.state === REJECTED) {
+    return this;
+  }
+  var promise = new this.constructor(INTERNAL);
+  if (this.state !== PENDING) {
+    var resolver = this.state === FULFILLED ? onFulfilled : onRejected;
+    unwrap(promise, resolver, this.value);
+  } else {
+    this.queue.push(new QueueItem(promise, onFulfilled, onRejected));
+  }
+  return promise;
+};
+
+Promise.prototype.catch = function (onRejected) {
+  return this.then(null, onRejected);
+};
+
+function QueueItem(promise, onFulfilled, onRejected) {
+  this.promise = promise;
+  this.callFulfilled = function (value) {
+    doResolve(this.promise, value);
+  };
+  this.callRejected = function (error) {
+    doReject(this.promise, error);
+  };
+  if (isFunction(onFulfilled)) {
+    this.callFulfilled = function (value) {
+      unwrap(this.promise, onFulfilled, value);
+    };
+  }
+  if (isFunction(onRejected)) {
+    this.callRejected = function (error) {
+      unwrap(this.promise, onRejected, error);
+    };
+  }
+}
+
+function unwrap(promise, func, value) {
+  immediate(function () {
+    var returnValue;
+    try {
+      returnValue = func(value);
+    } catch (error) {
+      return doReject(promise, error);
+    }
+    if (returnValue === promise) {
+      doReject(promise, new TypeError('Cannot resolve promise with itself'));
+    } else {
+      doResolve(promise, returnValue);
+    }
+  });
+}
+
+function doResolve(self, value) {
+  try {
+    var thenable = getThen(value);
+    if (thenable) {
+      safelyResolveThenable(self, thenable);
+    } else {
+      self.state = FULFILLED;
+      self.value = value;
+      self.queue.forEach(function (queueItem) {
+        queueItem.callFulfilled(value);
+      });
+    }
+    return self;
+  } catch (error) {
+    return doReject(self, error);
+  }
+}
+
+function doReject(self, error) {
+  self.state = REJECTED;
+  self.value = error;
+  self.queue.forEach(function (queueItem) {
+    queueItem.callRejected(error);
+  });
+  return self;
+}
+
+function getThen(obj) {
+  var then = obj && obj.then;
+  if (obj && (isObject(obj) || isFunction(obj)) && isFunction(then)) {
+    return function appyThen() {
+      then.apply(obj, arguments);
+    };
+  }
+}
+
+function safelyResolveThenable(self, thenable) {
+  var called = false;
+  try {
+    thenable(function (value) {
+      if (called) {
+        return;
+      }
+      called = true;
+      doResolve(self, value);
+    }, function (error) {
+      if (called) {
+        return;
+      }
+      called = true;
+      doReject(self, error);
+    });
+  } catch (error) {
+    if (called) {
+      return;
+    }
+    called = true;
+    doReject(self, error);
+  }
+}
+
+Promise.resolve = resolve;
+function resolve(value) {
+  if (value instanceof this) {
+    return value;
+  }
+  return doResolve(new this(INTERNAL), value);
+}
+
+Promise.reject = reject;
+function reject(reason) {
+  var promise = new this(INTERNAL);
+  return doReject(promise, reason);
+}
+
+Promise.all = all;
+function all(iterable) {
+  var self = this;
+  if (!isArray(iterable)) {
+    return this.reject(new TypeError('must be an array'));
+  }
+
+  var len = iterable.length;
+  var called = false;
+  if (!len) {
+    return this.resolve([]);
+  }
+
+  var values = new Array(len);
+  var resolved = 0;
+  var i = -1;
+  var promise = new this(INTERNAL);
+
+  while (++i < len) {
+    allResolver(iterable[i], i);
+  }
+  return promise;
+  function allResolver(value, i) {
+    self.resolve(value).then(resolveFromAll, function (error) {
+      if (!called) {
+        called = true;
+        doReject(promise, error);
+      }
+    });
+    function resolveFromAll(outValue) {
+      values[i] = outValue;
+      if (++resolved === len && !called) {
+        called = true;
+        doResolve(promise, values);
+      }
+    }
+  }
+}
+
+Promise.race = race;
+function race(iterable) {
+  var self = this;
+  if (!isArray(iterable)) {
+    return this.reject(new TypeError('must be an array'));
+  }
+
+  var len = iterable.length;
+  var called = false;
+  if (!len) {
+    return this.resolve([]);
+  }
+
+  var i = -1;
+  var promise = new this(INTERNAL);
+
+  while (++i < len) {
+    resolver(iterable[i]);
+  }
+  return promise;
+  function resolver(value) {
+    self.resolve(value).then(function (response) {
+      if (!called) {
+        called = true;
+        doResolve(promise, response);
+      }
+    }, function (error) {
+      if (!called) {
+        called = true;
+        doReject(promise, error);
+      }
+    });
+  }
+}
